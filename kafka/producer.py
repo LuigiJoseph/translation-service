4d22eafd7e855426f51654adad5d32017ead8b39
Mohammed <luigiornojoseph@gmail.com>
2025-01-29 15:41:12 +0300

    consumer consumer made

---
+++ kafka/producer.py
@@ -0,0 +1,20 @@
+from confluent_kafka import Producer
+import json
+import os
+
+
+# Load kafka configration
+KAFKA_BROKER  = os.getenv("KAFKA_BROKER", "kafka:9093")
+TOPIC_IN = "topic_in"
+
+# Initilize the kafka producer
+producer = Producer({'bootstrap.servers:': KAFKA_BROKER})
+
+# Function to send message
+def send_translation_request(text):
+    data = json.dumps({"text": text})
+    producer.produce(TOPIC_IN, value=data)
+    producer.flush()
+    print(f"Successfuly Sent translation request: {data}")
+
+
