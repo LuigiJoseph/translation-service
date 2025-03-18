2d7d00059b2259e1464f40e51b98d2471e91dae8
hanouf-translate <Alhanouftranslation@outlook.com>
2025-03-18 16:04:00 +0300

    edited comments in translation_methods

--- model_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-rest_api_url = "http://localhost:5000/translation-endpoints/api/v1/translate/helsinki/en/tr"
-payload = {"text": "coffee"}
-
-response = requests.post(rest_api_url, json=payload, headers={"Content-Type": "application/json"}, timeout=5)
-
-print("Status Code:", response.status_code)
-print("Response:", response.json())
--- python_services/sync/translation_methods.py
+++ python_services/sync/translation_methods.py
@@ -1,4 +1,3 @@
-
 from python_services.sync.log.loggers import logger
 from python_services.sync.models.transformers_models import translate_text 
 from python_services.sync.models.qwen_model import translate_qwen
@@ -12,6 +11,11 @@ logger.info(f"Available models: {MODEL_HANDLERS.keys()}")
    
 def translate_with_cache(text, source_locale, target_locale, model_name):
 
+
+    model_key = model_name.lower()
+    if model_key not in MODEL_HANDLERS:
+        return {"error": f"Model '{model_name}' is not supported."}, 400
+
     # Check if translation exists in cache
     cache_key = generate_cache_key(text, source_locale, target_locale, model_name)
 
@@ -22,26 +26,18 @@ def translate_with_cache(text, source_locale, target_locale, model_name):
         logger.info(f"Returning cached translation: {translated_text}")
         return translated_text
 
-    # logger.info("Performing translation")
-
-    model_key = model_name.lower()
-    if model_key not in MODEL_HANDLERS:
-        return {"error": f"Model '{model_name}' is not supported."}, 400
-    
     translation_function = MODEL_HANDLERS[model_key]
 
     if model_key == 'helsinki' and (source_locale, target_locale) in [("tr", "ar"), ("ar", "tr")]:
-        # Step 1: Translate to English (`en`)
-        intermediate_result = translation_function(text, source_locale, "en")
+
+        intermediate_result = translation_function(text, source_locale, "en") # Translate to English (`en`) intermediate language
         logger.info(f"Intermediate result: {intermediate_result} (type: {type(intermediate_result)})")
 
-        # No need for the dictionary check, just use the result directly
         intermediate_text = intermediate_result
         if not intermediate_text:
             return {"error": "Intermediate translation to English failed."}, 400
 
-        # Step 2: Translate from English (`en`) to the target language
-        final_result = translation_function(intermediate_text, "en", target_locale)
+        final_result = translation_function(intermediate_text, "en", target_locale) # Translate from English (`en`) to the target language
         logger.info(f"Final result: {final_result} (type: {type(final_result)})")
 
         if not final_result:
@@ -56,7 +52,3 @@ def translate_with_cache(text, source_locale, target_locale, model_name):
     cache_translations(cache_key,text, source_locale, target_locale, model_name, translated_text)
 
     return  translated_text
-    # else:
-    #     logger.error("Translation failed or model returned empty text.")
-
-    #     return {"Translation failed or model returned empty text"}, 400
